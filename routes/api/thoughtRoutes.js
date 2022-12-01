3181274780f5d2f0475aa1f31aeb86ac7edec022
Bekka <music_composer23@hotmail.com>
2022-12-01 03:13:33 -0600

    completed both routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtControllers');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reaction').post(addReaction);
+
+router.route('/:thoughtId/reaction/reactionId').delete(removeReaction);
+
+module.exports = router;
